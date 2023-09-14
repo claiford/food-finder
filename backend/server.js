278c7fcc994732a52224a1bc904a6e76b63d6829
Clifford Lok <cliffordcljw@gmail.com>
2023-09-14 19:05:33 +0800

    add: socket.io connection

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const http = require('http');
+const { Server } = require("socket.io");
 // const path = require('path');
 // const cookieParser = require('cookie-parser');
 // const logger = require('morgan');
 
-require('dotenv').config();
-// connect to the database with AFTER the config vars are processed
-require('./config/database');
-
-// Router import
+require('dotenv').config(); // process config vars => procces.env.VAR
+require('./config/database'); // connect to the database with AFTER the config vars are processed
 
 const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.FRONTEND_URL,
+    }
+});
 
 // app.use(logger('dev'));
 app.use(express.json());
@@ -39,7 +44,6 @@ app.use(express.urlencoded({ extended: false }));
 // });
 
 ///// TEST DB-CONNECTION
-
 const TestSchema = new mongoose.Schema({});
 const Test = mongoose.model('Test', TestSchema)
 
@@ -52,10 +56,19 @@ app.get('/', async function (req, res) {
         console.log(err);
     }
 })
-
 ///// TEST DB-CONNECTION
 
-app.listen(process.env.PORT, () => {
+///// TEST SOCKET
+io.on('connection', socket => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+})
+///// TEST SOCKET
+
+httpServer.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
 });
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 
 import axios from 'axios';
 
+const { io } = require("socket.io-client");
+console.log(process.env.REACT_APP_BACKEND_URL)
+const socket = io.connect(process.env.REACT_APP_BACKEND_URL)
+
 function App() {
   const getData = async () => {
     const res = await axios.get('http://localhost:3000/');
