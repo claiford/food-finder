3330b623a6d7757480d47ff2b3efd02477519c13
vivienphang <vivien.phang@lionsbot.com>
2023-09-14 17:58:52 +0800

    feat: setup CORS in BE, added FE pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,35 @@
-import { useEffect } from 'react';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import Main from "./pages/Main";
 
-import { Button } from '@mui/material';
-import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
-
-import axios from 'axios';
+import axios from "axios";
+import { Box } from "@mui/material";
 
 function App() {
   const getData = async () => {
-    const res = await axios.get('http://localhost:3000/');
+    const res = await axios.get(process.env.REACT_APP_BACKEND_URL);
     console.log(res.data[0].data);
-  }
+  };
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   return (
-    <div className="App">
-      <h1>React Frontend</h1>
-      <Button variant="text">Text</Button>
-      <AccessTimeFilledIcon />
-    </div>
+    <Box
+      sx={{
+        p: "20px",
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>App Name</h1>
+      <Main />
+    </Box>
   );
 }
 
