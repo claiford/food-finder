05d5f2a677d7de6eaac85a24c2b1709898725889
Clifford Lok <cliffordcljw@gmail.com>
2023-09-19 19:38:37 +0800

    add: form fields for creating new session

--- frontend/src/components/NewSession.jsx
+++ frontend/src/components/NewSession.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, TextField, Button, IconButton, Modal, CircularProgress, Typography } from '@mui/material';
+import { Box, TextField, Button, IconButton, Modal, CircularProgress, Typography, InputLabel, Select, MenuItem, FormControl } from '@mui/material';
 import axios from 'axios';
+import { usePlacesWidget } from "react-google-autocomplete";
 
 import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
 import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
@@ -25,31 +26,103 @@ const modalStyle = {
 const NewSession = () => {
     const [alertOpen, setAlertOpen] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [formError, setFormError] = useState({})
+    const [form, setForm] = useState({
+        location: null,
+        distance: null,
+        budget: null,
+    })
     const [newSession, setNewSession] = useState({})
     const navigate = useNavigate();
 
-    const handleModalClose = () => {
-        setAlertOpen(false);
-        navigate("/customer/group/groupid")
-    }
-
-    const createNewSession = async (e) => {
-        e.preventDefault()
-        try {
-            setLoading(true);
-            // setTimeout(() => {
-            //     setAlertOpen(true);
-            //     setLoading(false);
-            // }, 2000)
-            const res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/session/new`)
-            console.log("POST RES", res);
-            setNewSession(res.data)
-            setAlertOpen(true);
-            setLoading(false);
-        } catch (err) {
-            console.log(err)
+    const handleInputChange = (e, key) => {
+        if (key === "location") {
+            setForm((prevForm) => {
+                return {
+                    ...prevForm,
+                    location: null
+                }
+            });
+            setFormError((prevFormError) => {
+                return {
+                    ...prevFormError,
+                    location: "invalid"
+                }
+            });
+        } else {
+            setForm((prevForm) => {
+                return {
+                    ...prevForm,
+                    [key]: e.target.value
+                }
+            });
+            setFormError((prevFormError) => {
+                return {
+                    ...prevFormError,
+                    [key]: ""
+                }
+            });
         }
     }
+
+    const handleModalClose = () => {
+        setAlertOpen(false);
+        // navigate("/customer/group/groupid")
+    }
+
+    const handleSubmitForm = async (e) => {
+        e.preventDefault()
+        if (form.location && form.distance && form.budget) {
+            try {
+                setLoading(true);
+                // setTimeout(() => {
+                //     setAlertOpen(true);
+                //     setLoading(false);
+                // }, 2000)
+                // const res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/session/new`)
+                // console.log("POST RES", res);
+                // setNewSession(res.data)
+                setAlertOpen(true);
+                setLoading(false);
+            } catch (err) {
+                console.log(err)
+            }
+        } else {
+            for (const field of Object.keys(form)) {
+                if (form[field] === null) {
+                    console.log("FIELD CHECK", field)
+                    setFormError((prevFormError) => {
+                        return {
+                            ...prevFormError,
+                            [field]: "invalid"
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    const { ref: autocompleteRef } = usePlacesWidget({
+        apiKey: "AIzaSyBLJXCbjB7HjCUmosPIocJkCpGjno-WJLg",
+        onPlaceSelected: (place) => {
+            setForm((prevForm) => {
+                return {
+                    ...prevForm,
+                    location: place.place_id
+                }
+            });
+            setFormError((prevFormError) => {
+                return {
+                    ...prevFormError,
+                    location: ""
+                }
+            });
+        },
+        options: {
+            types: ['establishment'],
+            componentRestrictions: { country: 'sg' },
+        },
+    });
 
     return (
         <>
@@ -65,30 +138,67 @@ const NewSession = () => {
                 </Box>
             </Modal>
             <h1>Create Session</h1>
-            <form onSubmit={createNewSession}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                    fullWidth
-                    size="large"
-                    disabled={loading}
-                >
-                    Create
-                    {loading && (
-                        <CircularProgress
-                            size={24}
-                            sx={{
-                                position: 'absolute',
-                                top: '50%',
-                                left: '50%',
-                                marginTop: '-12px',
-                                marginLeft: '-12px',
-                            }}
-                        />
-                    )}
-                </Button>
-
+            <form onSubmit={handleSubmitForm}>
+                <FormControl>
+                    <TextField
+                        sx={{ my: 2 }}
+                        inputRef={autocompleteRef}
+                        label="Location"
+                        error={formError.location ? true : false}
+                        helperText={formError.location ? "Please select a valid location." : ""}
+                        onChange={(e) => handleInputChange(e, "location")}
+                    />
+                    <TextField
+                        select
+                        sx={{ my: 2 }}
+                        label="Distance"
+                        margin="normal"
+                        error={formError.distance ? true : false}
+                        helperText={formError.distance ? "Please select an option." : ""}
+                        onChange={(e) => handleInputChange(e, "distance")}
+                    >
+                        <MenuItem value={1}>{"< 1km"}</MenuItem>
+                        <MenuItem value={2}>{"< 2km"}</MenuItem>
+                        <MenuItem value={3}>{"< 3km"}</MenuItem>
+                    </TextField>
+                    <TextField
+                        select
+                        sx={{ my: 2 }}
+                        label="Budget"
+                        margin="normal"
+                        error={formError.budget ? true : false}
+                        helperText={formError.budget ? "Please select an option." : ""}
+                        onChange={(e) => handleInputChange(e, "budget")}
+                    >
+                        <MenuItem value={4}>Any</MenuItem>
+                        <MenuItem value={1}>$</MenuItem>
+                        <MenuItem value={2}>$$</MenuItem>
+                        <MenuItem value={3}>$$$</MenuItem>
+                    </TextField>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        fullWidth
+                        size="large"
+                        disabled={loading}
+                        sx={{ marginTop: "1rem", marginBottom: "2rem" }}
+                    >
+                        Create
+                        {loading && (
+                            <CircularProgress
+                                size={24}
+                                sx={{
+                                    position: 'absolute',
+                                    top: '50%',
+                                    left: '50%',
+                                    marginTop: '-12px',
+                                    marginLeft: '-12px',
+                                }}
+                            />
+                        )}
+                    </Button>
+                </FormControl>
             </form>
         </>
 
