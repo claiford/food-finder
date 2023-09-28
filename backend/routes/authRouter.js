6a7ef66bad4707031316038883a11526c2996021
Clifford Lok <cliffordcljw@gmail.com>
2023-09-28 21:06:04 +0800

    fix: rename authRouter.js to AuthRouter.js

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const AuthController = require("../controllers/AuthController");
+const AuthController = require("../controllers/AuthController"); //
 const passport = require("passport");
 // const local = require("./strategy/local")
 
